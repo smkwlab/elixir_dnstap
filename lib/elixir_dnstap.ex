d6233e30169f270e4ec461765fdf639ce7e6eda1
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2025-10-09 10:57:36 +0900

    Initial commit: mix new elixir_dnstap --sup

---
+++ lib/elixir_dnstap.ex
@@ -0,0 +1,18 @@
+defmodule ElixirDnstap do
+  @moduledoc """
+  Documentation for `ElixirDnstap`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirDnstap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_dnstap_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirDnstapTest do
+  use ExUnit.Case
+  doctest ElixirDnstap
+
+  test "greets the world" do
+    assert ElixirDnstap.hello() == :world
+  end
+end
